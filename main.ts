999f77ad154f300628fcc389d2e1d9c9f437793e
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-08-23 12:57:35 -0700

    access modifiers

--- main.ts
+++ main.ts
@@ -83,7 +83,13 @@
 // newThing.y = 17;
 // newThing.draw();
 
-// Below is how to use a Constructor in TypeScript. Constructors are methods that are called every time you create a new instance of a class 'constructor' is a reserved keyword in TS.
+// Below is how to use a Constructor in TypeScript. Constructors are methods that are called every time you create a new instance of a class 'constructor' is a reserved keyword in TS. You can make paramaters optional by appending 
+// a '?' within the paramater definition area. for example if we wanted to make the arguments optional values in the constructor below, it would look like this:
+
+// constructor(x?: number, y?: number){
+//     this.x =x;
+//     this.y = y;
+// }
 
 class Something {
     x: number;
@@ -105,3 +111,30 @@ let fred = new Something(1,2);
 
 fred.draw();
 
+
+
+//An 'Access Modifier' allows you to allow how 'mutiable' pieces of an object are within the class definition. For example, if you wanted variable 'x' to never be modified ever,
+// but you wanted variable 'y' to be muted at some point publically. In TS, the 3 Access Modifiers are 'Public, Private, and Protected'. Public and Private are most common. By default they are all Public
+
+// You can make a variable private in class defination by using the keyword 'private, as below. X is private. X will not be accessable. 
+//You can set access modifiers on fields, properties and methods
+// The getX() method will be able to be called from the outside and return value of x
+
+// class AnotherThingy {
+//     private x: number;
+//     y: number;
+
+    //constructor(private x?: number, private y?: number){
+
+    // }
+
+    // getX(){
+    //     return this.x;
+    // }
+// }
+
+// let anExample = new AnotherThingy(2,5);
+
+// will sucesfully return value of x even thought it is private.
+
+//  
